acd2159873016782e1bf910fb49b3b803a9aab6b
nicolastrres <nicolastrres@gmail.com>
2017-11-11 16:23:33 -0300

    List files of directory

---
+++ back_me_up.py
@@ -0,0 +1,5 @@
+import os
+
+
+def list_files(folder):
+    return os.listdir(folder)
---
+++ tests/test_back_me_up.py
@@ -0,0 +1,22 @@
+import tempfile
+
+import os
+from hamcrest import assert_that, equal_to
+
+import back_me_up
+
+
+def test_should_return_empty_list_when_no_files():
+    with tempfile.TemporaryDirectory() as directory:
+        actual_files = back_me_up.list_files(directory)
+
+    assert_that([], equal_to(actual_files))
+
+
+def test_should_return_all_files():
+    with tempfile.TemporaryDirectory() as directory:
+        with tempfile.NamedTemporaryFile(dir=directory) as file:
+            expected_file_name = os.path.basename(file.name)
+            actual_files = back_me_up.list_files(directory)
+
+    assert_that([expected_file_name], equal_to(actual_files))
