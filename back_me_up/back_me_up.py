eee9d00cf7241a96eb5780a88e9ac4622cc1fe73
nicolastrres <nicolastrres@gmail.com>
2017-11-11 19:54:28 -0300

    Get last modification date of file

--- back_me_up/back_me_up.py
+++ back_me_up/back_me_up.py
@@ -3,3 +3,7 @@ import os
 
 def list_files(folder):
     return os.listdir(folder)
+
+
+def get_last_modification_date(file):
+    return os.path.getmtime(file)
--- tests/test_back_me_up.py
+++ tests/test_back_me_up.py
@@ -1,6 +1,8 @@
 import tempfile
 
 import os
+from unittest.mock import Mock
+
 from hamcrest import assert_that, equal_to
 
 from back_me_up import back_me_up
@@ -20,3 +22,13 @@ def test_should_return_all_files():
             actual_files = back_me_up.list_files(directory)
 
     assert_that([expected_file_name], equal_to(actual_files))
+
+
+def test_should_return_last_modified_date():
+    expected_last_modification_date = 1510431915.0
+    os.path.getmtime = Mock(return_value=expected_last_modification_date)
+
+    with tempfile.NamedTemporaryFile() as file:
+        last_modified_date = back_me_up.get_last_modification_date(file.name)
+
+    assert_that(expected_last_modification_date, equal_to(last_modified_date))
