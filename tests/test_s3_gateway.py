a74fd30a6b12af3a78b792c6104c0cd2a2934274
Nicolas Agustin <nagustin@thoughtworks.com>
2018-10-12 13:43:05 -0300

    Use pytest in s3_gateway tests to keep consistency across the project

--- tests/test_s3_gateway.py
+++ tests/test_s3_gateway.py
@@ -1,13 +1,15 @@
-import unittest
+import boto3
+import pytest
 
 from unittest.mock import Mock
-import boto3
+from hamcrest import assert_that, equal_to, has_length
+
 
 from back_me_up.s3_gateway.s3_gateway import S3Gateway
 from back_me_up.s3_gateway.errors import UploadFileError
 
 
-class TestS3Client(unittest.TestCase):
+class TestS3Client:
 
     def test_upload_file(self):
         s3_client = Mock(spec=boto3.client('s3'))
@@ -25,7 +27,7 @@ class TestS3Client(unittest.TestCase):
         s3_client.upload_file.side_effect = boto3.exceptions.S3UploadFailedError('Some Error Message')
         s3_gateway = S3Gateway(client=s3_client)
 
-        with self.assertRaises(UploadFileError) as error_raised:
+        with pytest.raises(UploadFileError) as error_raised:
             s3_gateway.upload('MyBucket', 'some_file.txt')
 
-        self.assertEqual(error_raised.exception.args, ('Some Error Message',))
+        assert_that(error_raised.value.args, equal_to(('Some Error Message',)))
